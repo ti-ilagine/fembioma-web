c8a9f6be106895015ad17817337d95f4e1dee45e
ti-ilagine <ti@ilagine.com>
2026-09-08 12:58:45 -0500

    Update config.ts
    cambio a 15 de set

--- lib/config.ts
+++ lib/config.ts
@@ -2,4 +2,4 @@
 export const REGISTRATION_URL =
   'https://forms.cloud.microsoft/r/M8j6hK4KHu'
 
-export const EARLY_BIRD_DEADLINE = '31 de Agosto'
+export const EARLY_BIRD_DEADLINE = '15 de Setiembre'
